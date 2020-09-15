b9662b394b6dbf6939f11f69b5a8cb3b271dfec4
William <William@DESKTOP-CML67BM>
2020-09-15 18:15:50 -0400

    added author

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <link href="css/css.css" rel="stylesheet">
+    <meta charset="utf-8" name="author" content="William Forrester">
+  <link href="frameworks/css/css.css" rel="stylesheet">
 </head>
 <body>
-    <p> hello world hi</p>
+    <p> hello world hi </p>
 </body>
 </html>
