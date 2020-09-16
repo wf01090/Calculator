e2a63f31ce6fecc229ab7f5e6601f62c3eff1232
William <William@DESKTOP-CML67BM>
2020-09-16 18:38:55 -0400

    added title added main added table and table elements

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" name="author" content="William Forrester">
-  <link href="frameworks/css/css.css" rel="stylesheet">
+    <title>Forrester_Calculator</title>
+    <link href="frameworks/css/css.css" rel="stylesheet">
 </head>
 <body>
-    <p> hello world hi </p>
+    <main>
+        <table>
+            <tr>
+                <td></td>
+            </tr>
+            
+            
+        </table>
+        
+    </main>
 </body>
 </html>
