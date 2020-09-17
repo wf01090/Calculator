81e6d2a8942aef49809ee5f9a877caa061a131a8
William <William@DESKTOP-CML67BM>
2020-09-16 20:38:50 -0400

    Added the rest of the calculator buttons formatted the css for the buttons

--- index.php
+++ index.php
@@ -10,11 +10,32 @@
             <input type="text" id="in-out" >
         <table>
             <tr>
-                <td><input type="button" id="clear" value="C"></td>
-                <td><input type="button" id="del" value="Del">
+                <td id="r1" colspan="2"><input type="button" id="clear" value="C"></td>
+                <td id="r1" colspan="2"><input type="button" id="del" value="Del"></td>
             </tr>
             <tr>
-                
+                <td id="r2"><input type="button" id="7" value="7"></td>
+                <td id="r2"><input type="button" id="8" value="8"></td>
+                <td id="r2"><input type="button" id="9" value="9"></td>
+                <td id="r2"><input type="button" id="plus" value="+"></td>
+            </tr>
+            <tr>
+                <td id="r3"><input type="button" id="4" value="4"></td>
+                <td id="r3"><input type="button" id="5" value="5"></td>
+                <td id="r3"><input type="button" id="6" value="6"></td>
+                <td id="r3"><input type="button" id="minus" value="-"></td>
+            </tr>
+            <tr>
+                <td id="r4"><input type="button" id="1" value="1"></td>
+                <td id="r4"><input type="button" id="2" value="2"></td>
+                <td id="r4"><input type="button" id="3" value="3"></td>
+                <td id="r4"><input type="button" id="times" value="*"></td>
+            </tr>
+            <tr>
+                <td id="r5"><input type="button" id="point" value="."></td>
+                <td id="r5"><input type="button" id="0" value="0"></td>
+                <td id="r5"><input type="button" id="^" value="^"></td>
+                <td id="r5"><input type="button" id="divide" value="/"></td>
             </tr>
             
             
