b9bf94acdadf0034570daadeb2b557753797887e
William <William@DESKTOP-CML67BM>
2020-09-16 23:20:46 -0400

    Started adding to the JavaScript

--- js/javascritp.js
+++ js/javascritp.js
@@ -1,1 +1,12 @@
-
+var btn1 = document.getElementById("1");
+if(btn1.addEventListener){
+    btn1.addEventListener("click", function(){alert("hello");}, false);
+}else if (btn1.attachEvent){
+    btn1.attachEvent("onclick", function(){alert("hi");});
+}
+function textboxInput(x){
+    alert("in the function");
+    var y = document.getElementById("in-out").value;
+    alert(y);
+    document.getElementById("in-out").value = z;
+}
