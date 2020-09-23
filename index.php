0b9cc507ff6ff72c0ec9f7aa8bc4da69a258d977
William <William@DESKTOP-CML67BM>
2020-09-23 16:54:05 -0400

    moved javascript tag to end of the body

--- index.php
+++ index.php
@@ -3,39 +3,38 @@
     <meta charset="utf-8" name="author" content="William Forrester">
     <title>Forrester_Calculator</title>
     <link href="frameworks/css/css.css" rel="stylesheet">
-    <script src="js/javascritp.js"></script>
 </head>
 <body>
     <main>
         <form>
-            <input type="text" id="in-out" >
+            <input type="text" id="in-out" value="0">
         <table>
             <tr>
                 <td id="r1" colspan="2"><input type="button" id="clear" value="C"></td>
                 <td id="r1" colspan="2"><input type="button" id="eql" value="="></td>
             </tr>
             <tr>
-                <td id="r2"><input type="button" id="7" value="7"></td>
-                <td id="r2"><input type="button" id="8" value="8"></td>
-                <td id="r2"><input type="button" id="9" value="9"></td>
+                <td id="r2"><input type="button" id="btn7" value="7"></td>
+                <td id="r2"><input type="button" id="btn8" value="8"></td>
+                <td id="r2"><input type="button" id="btn9" value="9"></td>
                 <td id="r2"><input type="button" id="plus" value="+"></td>
             </tr>
             <tr>
-                <td id="r3"><input type="button" id="4" value="4"></td>
-                <td id="r3"><input type="button" id="5" value="5"></td>
-                <td id="r3"><input type="button" id="6" value="6"></td>
+                <td id="r3"><input type="button" id="btn4" value="4"></td>
+                <td id="r3"><input type="button" id="btn5" value="5"></td>
+                <td id="r3"><input type="button" id="btn6" value="6"></td>
                 <td id="r3"><input type="button" id="minus" value="-"></td>
             </tr>
             <tr>
-                <td id="r4"><input type="button" id="1" value="1"></td>
-                <td id="r4"><input type="button" id="2" value="2"></td>
-                <td id="r4"><input type="button" id="3" value="3"></td>
+                <td id="r4"><input type="button" id="btn1" value="1"></td>
+                <td id="r4"><input type="button" id="btn2" value="2"></td>
+                <td id="r4"><input type="button" id="btn3" value="3"></td>
                 <td id="r4"><input type="button" id="times" value="*"></td>
             </tr>
             <tr>
                 <td id="r5"><input type="button" id="point" value="."></td>
-                <td id="r5"><input type="button" id="0" value="0"></td>
-                <td id="r5"><input type="button" id="^" value="^"></td>
+                <td id="r5"><input type="button" id="btn0" value="0"></td>
+                <td id="r5"><input type="button" id="power" value="^"></td>
                 <td id="r5"><input type="button" id="divide" value="/"></td>
             </tr>
             
@@ -43,5 +42,6 @@
         </table>
         </form>
     </main>
+        <script src="js/javascritp.js"></script>
 </body>
 </html>
